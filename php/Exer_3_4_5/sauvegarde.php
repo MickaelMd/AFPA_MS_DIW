2d71a25bcfc58c17bee05b074383f2159475e15f
MickaelMd <M1cka4l@protonmail.com>
2024-08-27 16:11:46 +0200

    add

---
+++ php/Exer_3_4_5/sauvegarde.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+
+$i = 0;
+while ($i < 500) { 
+    
+    $i ++;
+    echo ($i = $i ++) . "<p>Je dois faire des sauvegardes régulières de mes fichiers. </p>"; }
+
+  ?> 
+
+
+</body>
+</html>
+
